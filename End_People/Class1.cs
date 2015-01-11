6a6dfebb307b3dcaa3e967c6e7dc2951accd46d9
ioanlongjing <ioan.snickersnee@gmail.com>
2015-01-11 16:24:07 +0800

    The Final_Test_DataBuilding

---
+++ End_People/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace End_People
+{
+    class Class1
+    {
+        public string FirstName { get; set; }
+        public string Mail      { get; set; }
+        public string Number    { get; set; }
+        public string Color     { get; set; }
+        public string WebLink   { get; set;}
+
+        public Class1() { }
+
+        public Class1(string F, string eM, string N, string W, string c) 
+        {
+            FirstName = F;  /* 名字 */
+            Mail = eM;      /* Mail */
+            Number = N;     /* 電話 */
+            WebLink = W;    /* WebLink */
+            Color = c;      /* 顏色 */
+        }
+    }
+}
---
+++ End_People/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+ 
+
+namespace End_People
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Class1[] Teacher = 
+                                { 
+                                    new Class1("鄭林合", "lgn@mail.com","0911-111-111","gn.sju.com","Blue"),
+                                    new Class1("陳瑞希", "crx@mail.com","0922-222-222","crx.sju.com","Red"),
+                                    new Class1("陳日昌", "jct@mail.com","0933-333-333","jct.sju.com","Green"),
+                                    new Class1("高堅志", "jjk@mail.com","0944-444-444","jjk.sju.com","Pink")
+                                };
+
+
+            //listbox_1.ItemsSource = people;
+            DataContext = Teacher;
+        }
+    }
+}
